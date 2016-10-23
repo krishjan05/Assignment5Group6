b560e8d4b932c00aefe2bc5c4a567b77f94db4f8
krishjan05 <krishjan05@gmail.com>
2016-10-23 15:43:44 -0400

    Implemented the code to add the result to file.

--- Assignment5Group6/Assignment5Group6/MainWindow.xaml.cs
+++ Assignment5Group6/Assignment5Group6/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -22,6 +23,8 @@ namespace Assignment5Group6
     {
         const int INCREMENT_FACTOR = 2;
         const string ERROR_MESSAGE = "Please enter valid inputs.";
+        const string DIRNAME = "Project Result";
+        const string FILENAME = "result.txt";
         public MainWindow()
         {
             InitializeComponent();
@@ -30,6 +33,11 @@ namespace Assignment5Group6
         {
             int numberOfDays;
             int totalSalary = 1;
+            string path = System.Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+            string fullPath = System.IO.Path.Combine(path, DIRNAME);
+            Directory.CreateDirectory(fullPath);
+            string filePath = System.IO.Path.Combine(fullPath, FILENAME);
+            StreamWriter sw = File.CreateText(filePath);
             if (int.TryParse(txtNumberOfDay.Text, out numberOfDays))
             {
                 for (int i = 1; i <= numberOfDays; i++)
@@ -37,7 +45,9 @@ namespace Assignment5Group6
                     totalSalary = i * INCREMENT_FACTOR;
                     string finalResult = i + " " + totalSalary;
                     lstSalary.Items.Add(finalResult);
+                    sw.WriteLine(finalResult);
                 }
+                sw.Close();
             }
             else
                 MessageBox.Show(ERROR_MESSAGE);
@@ -45,6 +55,7 @@ namespace Assignment5Group6
         private void btnCalculate_Click(object sender, RoutedEventArgs e)
         {
             CalcSalary();
+            //saveToFile();
         }
 
         private void btnReset_Click(object sender, RoutedEventArgs e)
@@ -52,5 +63,18 @@ namespace Assignment5Group6
             lstSalary.Items.Clear();
             txtNumberOfDay.Text = "";
         }
+       /* public void saveToFile()
+        {
+            string path = System.Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+            string fullPath = System.IO.Path.Combine(path, DIRNAME);
+            Directory.CreateDirectory(fullPath);
+            string filePath = System.IO.Path.Combine(fullPath, FILENAME);
+            StreamWriter sw = File.CreateText(filePath);
+            foreach(string listItem in lstSalary.Items)
+            {
+                sw.WriteLine(finalResult);
+            }
+
+        }*/
     }
 }
